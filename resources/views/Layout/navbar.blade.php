72cd7800b909cddac3f00281d4caae75ea743c1c
BagasFX <109197138+BagasFX@users.noreply.github.com>
2024-06-05 23:00:33 +0700

    first commit

---
+++ resources/views/Layout/navbar.blade.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-transparant">
+    <div class="container">
+        <!--Logo-->
+        <a class="navbar-brand fs-4" href="#"><b>Bagas</b>Alfanto</a>
+        <!--Logo-->
+
+        <button class="navbar-toggler border-0 shadow-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!--sidebar-->
+        <div class="sidebar offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header text-white border-bottom">
+            <h5 class="offcanvas-title " id="offcanvasNavbarLabel"><b>Bagas</b>Alfanto</h5>
+            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li class="nav-item mx-2">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item mx-2">
+                <a class="nav-link" href="#">About Me</a>
+                </li>
+                <li class="nav-item mx-2">
+                <a class="nav-link" href="#">Portofolio</a>
+                </li>
+                <li class="nav-item mx-2">
+                <a class="nav-link" href="#">Contact Me</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        <!--sidebar-->
+    </div>
+  </nav>
+  <link rel="stylesheet" href="/css/layout.css">
